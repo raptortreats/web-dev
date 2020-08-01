68bc16598e7e3c2858d4e14a80ea742f2599e138
raptortreats <rochish.sree@gmail.com>
2020-07-31 23:14:21 -0700

    quiz

---
+++ JavaScript/primitive-data-types/variables-quiz.js
@@ -0,0 +1,16 @@
+let eggCount = 42;
+eggCount + 2;
+
+console.log(eggCount)
+
+const rating = 7.5;
+// rating = 8;
+
+console.log(rating)
+
+// what is the value of wind_speed?
+let wind_speed = 76;
+wind_speed += 5; // wind_speed = wind_speed + 5 assignment operATORS
+wind_speed--;
+
+console.log(wind_speed); // naming convention has to be changed otherwise it is valid too
