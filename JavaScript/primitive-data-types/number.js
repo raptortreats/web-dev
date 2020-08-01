2e3c2cb55a49ce1e91fa81695f9ac1db798d8905
raptortreats <rochish.sree@gmail.com>
2020-07-31 23:04:42 -0700

    numbers

---
+++ JavaScript/primitive-data-types/number.js
@@ -0,0 +1,18 @@
+console.log(typeof "100")
+console.log(typeof "100" + 100)
+console.log(typeof ("100" + 100))
+
+
+console.log(typeof 100)
+console.log(typeof -100)
+console.log(typeof 10.5)
+console.log(typeof -100.8)
+console.log(typeof 100)
+
+// simple mathematical operations
+
+console.log(5+7)
+console.log(7-0)
+console.log(4*9)
+console.log(7%5)
+console.log(8**5)
