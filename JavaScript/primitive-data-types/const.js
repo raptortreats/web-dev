33904efbe16add47f1450a33523a2745f77bd1b0
raptortreats <rochish.sree@gmail.com>
2020-07-31 23:26:30 -0700

    const

---
+++ JavaScript/primitive-data-types/const.js
@@ -0,0 +1,15 @@
+// const works just like let, except we can't change the value
+
+const age = 23;
+// age = 22
+
+console.log(age) // assignment to constant variable
+
+const score = 9;
+score+=1 // score =  score + 1
+console.log(score)
+
+
+const pi = 3.141
+const daysInWeek = 7;
+const minHeight = 60; 
