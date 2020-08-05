bf7d7f03eea48b9c852c061a2c34fc52c05c0221
raptortreats <rochish.sree@gmail.com>
2020-08-05 00:29:30 -0700

    pw validator using python

---
+++ Python/passwordvalidator.py
@@ -0,0 +1,9 @@
+password = None
+if(password == None):
+    print('password shoudnt be blank')
+elif(len(password)<6):
+    print('password shouldnt be less than 6 char')
+elif(" " in password):
+    print('password shouldnt contain spaces')  
+else:
+    print('valid password') 
