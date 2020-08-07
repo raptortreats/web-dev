ca896d82cdd8f4dc8e436e0c12ebf61d20da6b69
raptortreats <rochish.sree@gmail.com>
2020-08-06 23:12:53 -0700

    update

---
+++ JavaScript/loops/forloop.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 10; i++) {
+  console.log("hello", i);
+}
+
+// 0 < 10 true "hello"
+// 1 < 10 true "hello"
+
+for (let j = 10; j > 0; j--) {
+  console.log(j);
+}
+
+let animals = ["rochish", "santosh", "krishna", "nitin", "praneeth"];
+for (let k = animals.length - 1; k >= 0; k--) {
+  console.log(animals[k]);
+}
+
+for (let m = 0; m < 10; m++) {
+  console.log(m);
+}
---
+++ JavaScript/loops/whileloop.js
@@ -0,0 +1,11 @@
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i = i + 1;
+}
+
+let j = 10;
+while (j > 0) {
+  console.log(j);
+  j--;
+}
