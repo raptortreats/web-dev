de817e31e459121c57568256ff359dc6553069b6
raptortreats <rochish.sree@gmail.com>
2020-08-07 23:18:40 -0700

    dictionaries

---
+++ Python/dictionaries.py
@@ -0,0 +1,11 @@
+fruits = {"apple": 20, "mangoes": 10, "oranges": 15}
+print(fruits)
+# accesing items
+print(fruits["apple"])
+
+for x in fruits:
+    print(x)
+    print(fruits[x])
+
+
+print(len(fruits))  # length of dict
--- Python/passwordvalidator.py
+++ Python/passwordvalidator.py
@@ -1,9 +1,9 @@
-password = None
+password = "rffffhfhfhf"
 if(password == None):
     print('password shoudnt be blank')
-elif(len(password)<6):
+elif(len(password) < 6):
     print('password shouldnt be less than 6 char')
 elif(" " in password):
-    print('password shouldnt contain spaces')  
+    print('password shouldnt contain spaces')
 else:
-    print('valid password') 
+    print('valid password')
