cf26b7aeab538e3505a78215144bb596f60c5fcd
raptortreats <rochish.sree@gmail.com>
2020-08-28 17:14:10 -0700

    in progress

--- Python/OOPS/game.py
+++ Python/OOPS/game.py
@@ -1,22 +1,125 @@
-class game:
-    def __init__(self, name, health):
-        self.name = name
-        self.health = health
+import turtle
+import random
+import math
 
-    @property
-    def kill(self):
-        if self.health == 5:
-            print(self.health, "monster is alive")
-
-        elif self.health <= 3:
-            print(self.health, "monster got hit")
-
-        elif self.health == 0:
-            print(self.health, "monster is dead")
-
-        else:
-            print("you are out of options")
+sc = turtle.Screen()
+sc.bgcolor("grey")
+sc.title("**** the dot")
 
 
-hit1 = game("rochish", 5)
-print(hit1.kill)
+class Border(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.hideturtle
+        self.speed(0)
+        self.color("blue")
+        self.pensize(5)
+
+    def border_dimen(self):
+        self.penup()
+        self.goto(-300, -300)
+        self.pendown
+        self.goto(-300, 300)
+        self.goto(300, 300)
+        self.goto(300, -300)
+        self.goto(-300, -300)
+
+# killer
+
+
+class Player(turtle.Turtle):
+
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.speed(0)
+        self.shape("turtle")
+        self.color("black")
+        self.speed = 1
+
+    def move(self):
+        self.forward(self.speed)
+
+        if self.xcor() > 290 or self.xcor() < -290:
+            self.left(60)
+        if self.ycor() > 290 or self.ycor() < -290:
+            self.right(60)
+
+    def turnleft(self):
+        self.left(30)
+
+    def turnright(self):
+        self.right(30)
+
+    def acceleratespeed(self):
+        self.speed += 1
+
+    def deceleratespeed(self):
+        self.speed -= 1
+
+    def pausespeed(self):
+        self.speed = 0
+
+
+# victim
+class Dot(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.speed(0)
+        self.color("green")
+        self.shape("circle")
+        self.speed = 3
+        self.goto(random.randint(-250, 250), random.randint(-250, 250))
+        self.setheading(random.randint(0, 360))
+
+    def jump(self):
+        self.goto(random.randint(-250, 250), random.randint(-250, 250))
+        self.setheading(random.randint(0, 360))
+
+    def move(self):
+        self.forward(self.speed)
+
+        if self.xcor() > 290 or self.xcor() < -290:
+            self.left(60)
+        if self.ycor() > 290 or self.ycor() < -290:
+            self.right(60)
+
+    def pauses(self):
+        self.speed = 0
+
+
+def collision(t1, t2):
+    a = t1.xcor()-t2.xcor()
+    b = t1.ycor()-t2.ycor()
+    distance = math.sqrt((a**2)+(b**2))
+
+    if distance < 20:
+        return True
+    else:
+        return False
+
+
+play = Player()
+dot = Dot()
+border = Border()
+
+border.border_dimen()
+
+# keyboard assignin
+turtle.listen()
+turtle.onkey(play.turnleft, "Left")
+turtle.onkey(play.turnright, "Right")
+turtle.onkey(play.acceleratespeed, "Up")
+turtle.onkey(play.deceleratespeed, "Down")
+turtle.onkey(play.pausespeed, "space")
+#turtle.onkey(dot.pauses, "space")
+
+
+while True:
+    play.move()
+    dot.move()
+
+    if collision(play, dot):
+        dot.jump()
