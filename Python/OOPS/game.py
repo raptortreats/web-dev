51a14d99beb5ec09d15a2a3f8494628d955b2478
raptortreats <rochish.sree@gmail.com>
2020-08-29 17:27:06 -0700

    turtle kill

--- Python/OOPS/game.py
+++ Python/OOPS/game.py
@@ -1,10 +1,37 @@
 import turtle
 import random
 import math
+import os
+import winsound
 
 sc = turtle.Screen()
 sc.bgcolor("grey")
-sc.title("**** the dot")
+sc.title("**** the turts")
+sc.bgpic("tort.gif")
+# sc.register_shape("")
+
+
+class Pts(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup
+        self.hideturtle
+        self.speed(0)
+        self.color("black")
+        self.goto(-200, 230)
+        self.score = 0
+
+    def update_score(self):
+        self.clear()
+        self.write("Kills : {}".format(self.score), False,
+                   align="Left", font=("Ariel", 14, "normal"))
+
+    def change_score(self, points):
+        self.score += points
+        self.update_score()
+
+    def play_sound(self):
+        winsound.Beep(2000, 100)
 
 
 class Border(turtle.Turtle):
@@ -13,17 +40,17 @@ class Border(turtle.Turtle):
         self.penup()
         self.hideturtle
         self.speed(0)
-        self.color("blue")
+        self.color("black")
         self.pensize(5)
 
     def border_dimen(self):
         self.penup()
-        self.goto(-300, -300)
+        self.goto(-250, -250)
         self.pendown
-        self.goto(-300, 300)
-        self.goto(300, 300)
-        self.goto(300, -300)
-        self.goto(-300, -300)
+        self.goto(-250, 250)
+        self.goto(250, 250)
+        self.goto(250, -250)
+        self.goto(-250, -250)
 
 # killer
 
@@ -34,16 +61,16 @@ class Player(turtle.Turtle):
         turtle.Turtle.__init__(self)
         self.penup()
         self.speed(0)
-        self.shape("turtle")
+        self.shape("classic")
         self.color("black")
         self.speed = 1
 
     def move(self):
         self.forward(self.speed)
 
-        if self.xcor() > 290 or self.xcor() < -290:
+        if self.xcor() > 240 or self.xcor() < -240:
             self.left(60)
-        if self.ycor() > 290 or self.ycor() < -290:
+        if self.ycor() > 240 or self.ycor() < -240:
             self.right(60)
 
     def turnleft(self):
@@ -68,22 +95,22 @@ class Dot(turtle.Turtle):
         turtle.Turtle.__init__(self)
         self.penup()
         self.speed(0)
-        self.color("green")
-        self.shape("circle")
+        self.color("yellow")
+        self.shape("turtle")
         self.speed = 3
-        self.goto(random.randint(-250, 250), random.randint(-250, 250))
-        self.setheading(random.randint(0, 360))
+        self.goto(random.randint(-240, 240), random.randint(-240, 240))
+        self.setheading(random.randint(0, 270))
 
     def jump(self):
-        self.goto(random.randint(-250, 250), random.randint(-250, 250))
-        self.setheading(random.randint(0, 360))
+        self.goto(random.randint(-240, 240), random.randint(-240, 240))
+        self.setheading(random.randint(0, 270))
 
     def move(self):
         self.forward(self.speed)
 
-        if self.xcor() > 290 or self.xcor() < -290:
+        if self.xcor() > 240 or self.xcor() < -240:
             self.left(60)
-        if self.ycor() > 290 or self.ycor() < -290:
+        if self.ycor() > 240 or self.ycor() < -240:
             self.right(60)
 
     def pauses(self):
@@ -102,10 +129,16 @@ def collision(t1, t2):
 
 
 play = Player()
-dot = Dot()
+
 border = Border()
+pts = Pts()
 
 border.border_dimen()
+
+# multiple victims
+dots = []
+for i in range(6):
+    dots.append(Dot())
 
 # keyboard assignin
 turtle.listen()
@@ -116,10 +149,17 @@ turtle.onkey(play.deceleratespeed, "Down")
 turtle.onkey(play.pausespeed, "space")
 #turtle.onkey(dot.pauses, "space")
 
+sc.tracer(0)  # speed up the game
+
 
 while True:
+    sc.update()
     play.move()
-    dot.move()
 
-    if collision(play, dot):
-        dot.jump()
+    for dot in dots:
+        dot.move()
+
+        if collision(play, dot):
+            dot.jump()
+            pts.change_score(10)
+            pts.play_sound()
