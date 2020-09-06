827246593de2976eaa805ddd6919de6d548aa354
raptortreats <rochish.sree@gmail.com>
2020-09-05 18:01:16 -0700

    default values

---
+++ Python/Pythonicness and Packaging/DefaultValues.py
@@ -0,0 +1,8 @@
+def function(x, y, game="world of warcraft"):
+    print(game)
+
+
+function(2, 3)
+function(5, 6, "arkham origins")
+
+# named parameters can be made optional by giving them a default value
