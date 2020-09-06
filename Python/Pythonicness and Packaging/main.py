02564d39c77eacea2da03e1eea005c92fe3de34d
raptortreats <rochish.sree@gmail.com>
2020-09-05 18:59:24 -0700

    __main__

---
+++ Python/Pythonicness and Packaging/main.py
@@ -0,0 +1,9 @@
+# __main__ this can be used if we save it and it can imported using the file name
+
+
+def rochish():
+    print("i will kill you")
+
+
+if __name__ == "__main__":
+    print("hahahahah")
