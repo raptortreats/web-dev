5fa14039b94bad9d3275a886b068f119d9f33959
raptortreats <rochish.sree@gmail.com>
2020-09-05 18:51:06 -0700

    uses of else statements

---
+++ Python/Pythonicness and Packaging/ElseStatement.py
@@ -0,0 +1,14 @@
+# not only with if we can also use else with for and while too
+
+for i in range(10):
+    if i == 99:
+        break
+else:
+    print("no")
+
+
+for i in range(20):
+    if i == 10:
+        break
+else:
+    print("no")
