826775a21a7c467a5bdcd2d28b18ef022b8ffd5c
raptortreats <rochish.sree@gmail.com>
2020-09-05 18:06:07 -0700

    fucn arguments

--- Python/Pythonicness and Packaging/FuncArgs.py
+++ Python/Pythonicness and Packaging/FuncArgs.py
@@ -14,3 +14,12 @@ def my_games(my_name, *args):
 
 my_games("rtreats", "COD", "PUBG", "modern warfare",
          "resident evil", "ballistic", "WOW")
+
+
+# **KWARGS  allows you to hande named arguments that you have not defined in advance
+
+def my_func(x, y=7, *args, **kwargs):
+    print(kwargs)
+
+
+my_func(1, 2, 3, 4, s=8, r=10)
