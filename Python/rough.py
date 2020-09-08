e40bcea804c60eb8df845304816896727c64b084
raptortreats <rochish.sree@gmail.com>
2020-09-07 22:34:19 -0700

    rough

--- Python/rough.py
+++ Python/rough.py
@@ -1,4 +1,21 @@
 days = input("enter days")
 rate = input("rate per day")
-pay = (float(days) * float(rate))
-print(pay)
+#pay = (float(days) * float(rate))
+# print(pay)
+
+
+#astr = 'hdhdh'
+#istr = 0
+
+# try:
+#   istr = int(astr)
+
+# except:
+#   istr = -1
+
+try:
+    day = float(days)
+    rpd = float(rate)
+    print(day*rpd)
+except:
+    print("error,please input numeric value")
