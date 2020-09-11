7b6a046095714b2981433e65f3b6f4a76dd6f381
raptortreats <rochish.sree@gmail.com>
2020-09-10 19:06:31 -0700

    while

--- Python/whileloops.py
+++ Python/whileloops.py
@@ -1,17 +1,16 @@
-<<<<<<< HEAD
-a = [12,345,76,8,9]
+
+a = [12, 345, 76, 8, 9]
 i = 0
 while i < len(a):
-    
+
     if(a[i] < 22):
         print('karma')
     i = i + 1
-=======
-a = [1,234,876,45,23]
+
+a = [1, 234, 876, 45, 23]
 i = 0
 while i < len(a):
-   
-   if(a[i]<22):
-      print('karma')
-   i = i + 1     
->>>>>>> ec4c5c611afc155058446f5a57ad28723f367320
+
+    if(a[i] < 22):
+        print('karma')
+    i = i + 1
